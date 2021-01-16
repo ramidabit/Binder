e17d8421813183b93a8271f67d82ced2dfb7e983
Nandhan Natarajan <nandy@Nandhans-MacBook-Pro.local>
2021-01-16 12:57:19 -0800

    updated hello message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+    res.send('<h1>Waddup World!</h1>')
 });
 
 const PORT = process.env.PORT || 5000;
