af6b5e29eaabf4e452ca2196f5d60dfab37230fe
Nandhan Natarajan <nandy@Nandhans-MacBook-Pro.local>
2021-01-16 21:51:16 -0800

    added authorization and basic templating

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const firebase = require('firebase');
 const admin = require('firebase-admin');
 const fs = require('fs');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const handlebars = require('express-handlebars');
+
+const PORT = process.env.PORT || 5000;
+
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '1025752008731-9m28dve3ufam91gngdavvfsjefvr87db.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
+
 const serviceAccount = JSON.parse(fs.readFileSync('binder-52ad0-firebase-adminsdk-kt0pi-4c4c056db4.json'));
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -10,9 +20,15 @@ admin.initializeApp({
 const db = admin.firestore();
 
 
-app = express();
+let app = express();
 
-app.get('/', (req, res) => {
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get('/', (req, res)=>{
+    
     db.collection('users').add({
         name: 'Nandy',
         id: 69
@@ -21,9 +37,69 @@ app.get('/', (req, res) => {
     }).catch((error) => {
         console.error('Error creating document', error);
     }); 
-    res.send('<h1>Waddup</h1>');
-})
+    res.render('login');
+});
 
-const PORT = process.env.PORT || 5000;
+app.get('/login', (req,res)=>{
+    res.render('login');
+});
+
+app.post('/login', (req,res)=>{
+    let token = req.body.token;
+
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+      }
+      verify()
+      .then(()=>{
+          res.cookie('session-token', token);
+          res.send('success')
+      })
+      .catch(console.error);
+
+});
+app.get('/logout', (req, res)=>{
+    res.clearCookie('session-token');
+    res.redirect('/login')
+
+});
+app.get('/page', checkAuthenticated, (req, res) => {
+    let user = req.user;
+    res.render('page', {user});
+});
+
+
+function checkAuthenticated(req, res, next){
+
+    let token = req.cookies['session-token'];
+
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+      verify()
+      .then(()=>{
+          req.user = user;
+          next();
+      })
+      .catch(err=>{
+          res.redirect('/login')
+      })
+
+};
+
+
 
 app.listen(PORT);
