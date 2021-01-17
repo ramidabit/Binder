5f01b71680ac528f037e5e8e93d9831bbc68d5e7
Nandhan Natarajan <nandy@Nandhans-MacBook-Pro.local>
2021-01-16 16:51:33 -0800

    got firebase firestore to work

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const firebase = require('firebase');
+const admin = require('firebase-admin');
+const fs = require('fs');
+const serviceAccount = JSON.parse(fs.readFileSync('binder-52ad0-firebase-adminsdk-kt0pi-4c4c056db4.json'));
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+  
+const db = admin.firestore();
+
 
 app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Waddup World!</h1>')
-});
+    db.collection('users').add({
+        name: 'Nandy',
+        id: 69
+    }).then((doc)=>{
+        console.log('Document written with ID: ', doc.id);
+    }).catch((error) => {
+        console.error('Error creating document', error);
+    }); 
+    res.send('<h1>Waddup</h1>');
+})
 
 const PORT = process.env.PORT || 5000;
 
